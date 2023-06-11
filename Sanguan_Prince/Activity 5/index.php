fedd5c674d063511f48540176ed5a7cf064d9f13
Prince Sanguan <princesanguan44@gmail.com>
2023-06-11 16:11:02 +0800

    My Complete Activity 5

--- Sanguan_Prince/Activity 5/index.php
+++ Sanguan_Prince/Activity 5/index.php
@@ -1,11 +1,38 @@
 <?php
 
+$itemOne = 100;
+$itemTwo = 200;
+$itemThree = 300;
 
-echo getQuantityRemainingItem()
+echo getQuantityInItemOne(3);
+echo "\n";
+echo getQuantityInItemOne(5);
+echo "\n";
+echo getQuantityInItemTwo(5);
+echo "\n";
+echo getQuantityInItemOne(50);
+echo "\n";
+echo getQuantityInItemThree(5);
 
-function getQuantityRemainingItem($itemNumber, $qty, $remainingStock) {
-  $totalStock = $itemNumber - $qty;
-return "$itemNumber - ($qty) qty | $remainingStock = $totalStock";
+function getQuantityInItemOne($qty) {
+global $itemOne;
+$totalStock = $itemOne - $qty;
+$itemOne = $totalStock;
+return "Item 1 - ($qty) qty | Remaining Stocks = $totalStock";
+}
+
+function getQuantityInItemTwo($qty) {
+global $itemTwo;
+$totalStock = $itemTwo - $qty;
+$itemTwo = $totalStock;
+return "Item 2 - ($qty) qty | Remaining Stocks = $totalStock";
+}
+
+function getQuantityInItemThree($qty) {
+global $itemThree;
+$totalStock = $itemThree - $qty;
+$itemThree = $totalStock;
+return "Item 3 - ($qty) qty | Remaining Stocks = $totalStock";
 }
 
 ?>
